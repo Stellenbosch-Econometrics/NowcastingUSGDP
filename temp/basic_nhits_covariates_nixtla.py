1cb0b2cf18be5ff9e9298cf9e414695d0548b2ee
Dawie <djvanlill@gmail.com>
2023-03-15 12:22:45 +0200

    Rewrite of demo code

--- temp/basic_nhits_covariates_nixtla.py
+++ temp/basic_nhits_covariates_nixtla.py
@@ -2,52 +2,58 @@
 # Try and model using NeuralForecast package. NHITS model with covariates.
 
 # MWE for demonstration purposes
-
-# import necessary packages
+import logging
+import os
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from neuralforecast import NeuralForecast
+from statsmodels.tsa.ar_model import AutoReg
 from neuralforecast.models import NHITS
+from neuralforecast import NeuralForecast
 
-import logging
 logging.getLogger("pytorch_lightning").setLevel(logging.WARNING)
-
-# import data
-df = pd.read_csv('../data/FRED/blocked/vintage_2019_01.csv')
-# df2 = pd.read_csv('../data/FRED/blocked/vintage_2019_02.csv')
-# df3 = pd.read_csv('../data/FRED/blocked/vintage_2019_03.csv')
-# df4 = pd.read_csv('../data/FRED/blocked/vintage_2019_04.csv')
-
-# rename columns
-df = df.rename(columns={"year_quarter": "ds", "GDPC1": "y"})
+os.environ["KMP_DUPLICATE_LIB_OK"] = "TRUE"
 
 
-def nixtla_struct(df):
-    """Shape into the required structure for `neuralforecast` model
-
-    Args:
-        df (pd.DataFrame): Pandas DataFrame with columns 'ds' and 'y'
-    """
+def load_data(file_path):
+    df = pd.read_csv(file_path)
+    df = df.rename(columns={"year_quarter": "ds", "GDPC1": "y"})
     df["unique_id"] = np.ones(len(df))
     df["ds"] = pd.to_datetime(df["ds"])
-    df.insert(0, "unique_id", df.pop("unique_id"))
-    df.insert(1, "ds", df.pop("ds"))
-    df.insert(2, "y", df.pop("y"))
+    df = df[["unique_id", "ds", "y"] +
+            [col for col in df.columns if col not in ["unique_id", "ds", "y"]]]
+    return df
 
 
-nixtla_struct(df)
+def impute_missing_values(df_pc):
+    for col in df_pc.columns:
+        train = df_pc[df_pc[col].notnull()]
+        test = df_pc[df_pc[col].isnull()]
 
-# Main problems are
-# 1. Imputation of missing values
-# 2. Need to repeat the nowcasting process for each of the vintages (some iteration over the different files in the directory).
+        model = AutoReg(train[col], lags=2)
+        result = model.fit()
 
-# target vs covariates
-target_cols = ["unique_id", "ds", "y"]
-target_df = df[target_cols]
-covariates = df.drop(columns=target_cols)
+        forecast = result.predict(
+            start=len(train), end=len(train) + len(test) - 1)
+        df_pc.loc[test.index, col] = forecast
+    df_pc.fillna(method='bfill', inplace=True)
+    return df_pc
 
-# past vs future covariates
+
+def create_neural_forecast_model(horizon, pcc_list, fcc_list):
+    model = NHITS(h=horizon,
+                  input_size=10 * horizon,
+                  hist_exog_list=pcc_list,
+                  futr_exog_list=fcc_list,
+                  scaler_type='robust',
+                  max_steps=100)
+    return NeuralForecast(models=[model], freq='Q')
+
+
+df = load_data('../data/FRED/blocked/vintage_2019_01.csv')
+
+target_df = df[["unique_id", "ds", "y"]]
+covariates = df.drop(columns=["unique_id", "ds", "y"])
+
 missing_cols = covariates.columns[covariates.isnull().any()]
 past_covariates = covariates.filter(missing_cols)
 future_covariates = covariates.drop(columns=missing_cols)
@@ -55,32 +61,19 @@ future_covariates = covariates.drop(columns=missing_cols)
 pcc_list = past_covariates.columns.tolist()
 fcc_list = future_covariates.columns.tolist()
 
-# future covariates at same date as target
 df = pd.merge(target_df, future_covariates, left_index=True, right_index=True)
 
-# impute / forecast missing values -- this is the simplest approach.
-past_covariates.fillna(method='ffill', inplace=True)
-past_covariates.fillna(method='bfill', inplace=True)
-
-df = pd.merge(df, past_covariates, left_index=True, right_index=True)
+df_pc = impute_missing_values(past_covariates)
+df = pd.merge(df, df_pc, left_index=True, right_index=True)
 df = df.iloc[:-1]
 
-horizon = 2
-model = NHITS(h=horizon,
-              input_size=10*horizon,
-              hist_exog_list=pcc_list,
-              futr_exog_list=fcc_list,
-              scaler_type='robust',
-              max_steps=100  # epochs
-              )
-nf = NeuralForecast(models=[model], freq='Q')
+horizon = 5
+nf = create_neural_forecast_model(horizon, pcc_list, fcc_list)
 nf.fit(df=df)
 
 futr_df = pd.merge(target_df, future_covariates,
                    left_index=True, right_index=True)
-futr_df = futr_df.drop(columns="y")
-futr_df = futr_df.iloc[-1:]
+futr_df = futr_df.drop(columns="y").iloc[-1:]
 
 Y_hat_df = nf.predict(futr_df=futr_df)
-
 Y_hat_df
