66b9ee85d37cbe6fab82941d87e6e5f144d10642
Dawie <djvanlill@gmail.com>
2023-03-15 12:35:52 +0200

    Data imputation improved

--- temp/basic_nhits_covariates_nixtla.py
+++ temp/basic_nhits_covariates_nixtla.py
@@ -1,7 +1,6 @@
 
 # Try and model using NeuralForecast package. NHITS model with covariates.
 
-# MWE for demonstration purposes
 import logging
 import os
 import numpy as np
@@ -24,20 +23,40 @@ def load_data(file_path):
     return df
 
 
-def impute_missing_values(df_pc):
-    for col in df_pc.columns:
-        train = df_pc[df_pc[col].notnull()]
-        test = df_pc[df_pc[col].isnull()]
-
-        model = AutoReg(train[col], lags=2)
-        result = model.fit()
-
-        forecast = result.predict(
-            start=len(train), end=len(train) + len(test) - 1)
-        df_pc.loc[test.index, col] = forecast
-    df_pc.fillna(method='bfill', inplace=True)
-    return df_pc
-
+def impute_missing_values_ar_multiseries(data, lags=1):
+    """
+    Impute missing values in multiple time series using an autoregressive model.
+    
+    Args:
+        data (pd.DataFrame): The time series data with missing values to impute. Each column represents a time series.
+        lags (int): The number of lags to use in the autoregressive model.
+        
+    Returns:
+        pd.DataFrame: The imputed time series data.
+    """
+    imputed_data = data.copy()
+    
+    for col in data.columns:
+        while imputed_data[col].isnull().any():
+            not_null_indices = imputed_data[col].notnull()
+            train = imputed_data.loc[not_null_indices, col]
+            null_indices = imputed_data[col].isnull()
+            test_indices = imputed_data.loc[null_indices, col].index
+            
+            model = AutoReg(train, lags=lags)
+            result = model.fit()
+            
+            for index in test_indices:
+                if index - lags < 0:
+                    available_data = imputed_data.loc[:index - 1, col].values
+                else:
+                    available_data = imputed_data.loc[index - lags:index - 1, col].values
+                if np.isnan(available_data).any():
+                    continue
+                forecast = result.predict(start=len(train), end=len(train))
+                imputed_data.loc[index, col] = forecast.iloc[0]
+                
+    return imputed_data
 
 def create_neural_forecast_model(horizon, pcc_list, fcc_list):
     model = NHITS(h=horizon,
@@ -63,7 +82,7 @@ fcc_list = future_covariates.columns.tolist()
 
 df = pd.merge(target_df, future_covariates, left_index=True, right_index=True)
 
-df_pc = impute_missing_values(past_covariates)
+df_pc = impute_missing_values_ar_multiseries(past_covariates, lags=1)
 df = pd.merge(df, df_pc, left_index=True, right_index=True)
 df = df.iloc[:-1]
 
