37c828dfafba64cc62a828e913ed6aa29e3ff85b
Sebastian Krantz <sebastian.krantz@graduateinstitute.ch>
2023-06-23 21:06:30 +0200

    Update results in presentation.

--- results/combine_results.r
+++ results/combine_results.r
@@ -1,15 +1,21 @@
 library(fastverse)
-
+library(zoo)
 source("results/forecast_evaluation.R")
 
 DFM = fread("results/DFM/ALL_DFM_results_long.csv")
 DL = fread("results/deep_learning/tidy_results.csv") %>% 
-     fmutate(vintage = substr(vintage_file, 9, 15))
+     fmutate(vintage = sub("_", "-", substr(vintage_file, 9, 15)))
+ML = fread("results/machine_learning/tidy_svr.csv") %>% 
+     fmutate(vintage = sub("_", "-", substr(vintage_file, 9, 15)))
 GDP = fread("data/FRED/QD/vintage_2023_02.csv") %>% 
       fselect(year_quarter = V1, GDPC1) %>% 
       fmutate(year_quarter = as.integer(substr(year_quarter, 1, 4)) + (as.integer(substr(year_quarter, 6, 6))-1) / 4)
   
 nowcasts = DL %>% fselect(year_quarter, vintage_quarter, vintage, model = Model, value = Estimate) %>% 
+  rbind(ML %>% ftransform(year_quarter = unattrib(as.yearqtr(ds)),
+                          vintage_quarter = unattrib(as.yearqtr(as.Date(paste0(vintage, "-01")))), 
+                          Model = sub("svr", "SVR", tolower(Model))) %>% 
+          fselect(year_quarter, vintage_quarter, vintage, model = Model, value = Estimate)) %>% 
   rbind(DFM %>% fselect(year_quarter, vintage_quarter, vintage, model, value) %>% tfm(model = paste0("DFM_", model))) %>% 
   fmutate(trel = as.integer((year_quarter - vintage_quarter)*4)) %T>%
   with(print(varying(trel, model, any_group = FALSE))) %>% 
@@ -39,7 +45,7 @@ nowcasts_wide  = nowcasts[!model %ilike% "1se|o_min_"] %>%
   } %>% lapply(dcast, year_quarter ~ model, value.var = "value")
   
 
-outcome = GDP[ckmatch(avg_nowcast$year_quarter, year_quarter)]
+outcome = GDP[ckmatch(nowcasts_wide$average$year_quarter, year_quarter)]
 
 # Average nowcast (3 vintages per quarter)
 eval_forecasts(outcome$GDPC1 * 100, fselect(nowcasts_wide$average, -year_quarter) %c*% 100) %>% t() %>% qDF() %>% 
